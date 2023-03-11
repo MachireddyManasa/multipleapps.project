51a67d840df63973ca15d68307502047d7b8ac2d
MachireddyManasa <machireddymanasa03@gmail.com>
2023-03-11 15:19:50 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def manasa(request):
+    return render(request,'manasa.html')
---
+++ app2/urls.py
@@ -0,0 +1,6 @@
+from app2.views import *
+from django.urls import path
+app_name='manasa'
+urlpatterns=[
+path('pavan/',pavan,name='pavan'),
+]
